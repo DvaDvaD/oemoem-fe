b87c9bbaee362c27a8eb2b847f45f9f09d70efaa
DvaDvaD <davidlou0810@gmail.com>
2023-09-13 08:51:40 +0700

    update materi

--- js/index.js
+++ js/index.js
@@ -1,31 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector("button");
-  button.addEventListener("click", () => {
-    const box = document.querySelector(".box");
-    box.style.backgroundColor = "black";
-  });
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  const box = document.querySelector(".box");
+  box.style.backgroundColor = "black";
+});
 
-  const resetButton = document.querySelector("#reset-button");
-  resetButton.addEventListener("click", () => {
-    const box = document.querySelector(".box");
-    box.style.backgroundColor = "blueviolet";
-  });
+const resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", () => {
+  const box = document.querySelector(".box");
+  box.style.backgroundColor = "blueviolet";
+});
 
-  const submitButton = document.querySelector("#submit-button");
-  submitButton.addEventListener("click", () => {
-    const input = document.querySelector("input");
-    const hasilSubmit = document.querySelector("#hasil-submit");
-    hasilSubmit.innerHTML = input.value;
-  });
+const submitButton = document.querySelector("#submit-button");
+submitButton.addEventListener("click", () => {
+  const input = document.querySelector("input");
+  const hasilSubmit = document.querySelector("#hasil-submit");
+  hasilSubmit.innerHTML = input.value;
+});
 
-  const accordion = document.querySelector(".accordion");
-  accordion.addEventListener("click", () => {
-    const content = document.querySelector(".content");
+const tombol = document.querySelector(".tombol");
+tombol.addEventListener("click", () => {
+  const content = document.querySelector(".content");
 
-    if (content.style.display === "block") {
-      content.style.display = "none";
-      return;
-    }
+  if (content.style.display === "block") {
+    content.style.display = "none";
+    return;
+  } else {
     content.style.display = "block";
-  });
+  }
 });
